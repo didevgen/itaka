c229a19190288d53caddd3a5e3ed9fce428c4d49
maria.shpura <maria.shpura@sheerchain.com>
2019-08-15 01:33:42 +0300

    Create static Registration and Login page

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+  public authForm: FormGroup;
+  constructor(
+    private formBuilder: FormBuilder,
+  ) { }
+
+  ngOnInit() {
+    this.authForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+  onSubmit() {
+    console.log(this.authForm); // get data from form
+  }
+}
