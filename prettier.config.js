28aee4b8317e8dfe1668c239db298c75fc744264
Mykola Topchyi <mtopctc@training.local>
2019-08-20 13:26:09 +0300

    removed lint from precommit, changed prettier to double quotes

--- prettier.config.js
+++ prettier.config.js
@@ -2,7 +2,7 @@ module.exports = {
     printWidth: 80,
     semi: true,
     trailingComma: 'all',
-    singleQuote: true,
+    singleQuote: false,
     tabWidth: 4,
     bracketSpacing: true,
     jsxBracketSameLine: false,
