c6dc117103da8453dc0887142b71e946c624b9fe
heilikpd <heilikpd@gmail.com>
2019-08-22 00:08:41 +0300

    add main page components (v.1) and Montserrat font

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { HomeComponent } from './home/home.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material.module';
-
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./login/login.component";
+import { RegisterComponent } from "./register/register.component";
+import { HomeComponent } from "./home/home.component";
+import { ReactiveFormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MaterialModule } from "./material.module";
+import { HeaderComponent } from "./components/header/header.component";
+import { SidebarComponent } from "./components/sidebar/sidebar.component";
+import { ContentContainerComponent } from "./components/content-container/content-container.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    SidebarComponent,
+    ContentContainerComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
-    MaterialModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,18 +1,43 @@
-import { NgModule } from '@angular/core';
-import { MatButtonModule, MatCardModule, MatIconModule, MatInputModule } from '@angular/material';
-
+import { NgModule } from "@angular/core";
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatIconModule,
+  MatInputModule
+} from "@angular/material";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatBadgeModule } from "@angular/material/badge";
+import { MatListModule } from "@angular/material/list";
+import { MatRippleModule } from "@angular/material/core";
+import { MatTooltipModule } from "@angular/material/tooltip";
 @NgModule({
   imports: [
     MatInputModule,
     MatCardModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatGridListModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatBadgeModule,
+    MatListModule,
+    MatRippleModule,
+    MatTooltipModule
   ],
   exports: [
     MatInputModule,
     MatCardModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatGridListModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatBadgeModule,
+    MatListModule,
+    MatRippleModule,
+    MatTooltipModule
   ]
 })
 export class MaterialModule {}
