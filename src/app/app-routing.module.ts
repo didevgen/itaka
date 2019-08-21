4f634f03641d9b5c9c976edaa29ee4e16dbbe351
kerrimov <tolik.kerimov@gmail.com>
2019-08-21 16:52:21 +0300

    new file structure added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { RegisterComponent } from './register/register.component';
-import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
     { path: '', component: HomeComponent },
