8e3aca0290a004f9c8c07241e78e33a16a67e8e5
starosta1721 <starosta1721@gmail.com>
2019-08-23 12:01:21 +0300

    New attempt to ocreate google-auth

---
+++ src/app/components/auth/google-auth/google-auth.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import createAuth0Client from '@auth0/auth0-spa-js';
+
+@Component({
+  selector: 'app-google-auth',
+  templateUrl: './google-auth.component.html',
+  styleUrls: ['./google-auth.component.css']
+})
+export class GoogleAuthComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
