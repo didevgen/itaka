34fa819607acb73ab606732a4a58fd0c46e057de
heilikpd <heilikpd@gmail.com>
2019-08-27 18:29:11 +0300

    fixed bugs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,9 @@ import { MaterialModule } from './material.module';
 import { HeaderComponent } from './components/homepage/header/header.component';
 import { SidebarComponent } from './components/homepage/sidebar/sidebar.component';
 import { ContentContainerComponent } from './components/homepage/content-container/content-container.component';
-
+import {CardsContainerComponent} from './components/homepage/cards-container/cards-container.component';
+import {GoogleInWithAuth0Component} from './components/auth/googlein-with-auth0/googleIn-with-auth0.component';
+import {GoogleAuthComponent} from './components/auth/google-auth/google-auth.component';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireStorageModule } from 'angularfire2/storage';
 import { AngularFirestore } from 'angularfire2/firestore';
