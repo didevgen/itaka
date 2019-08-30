9b7c30a1b920a5a02aa3af9dc5e9a7773b7425d8
starosta1721 <starosta1721@gmail.com>
2019-08-30 14:40:40 +0300

    Make settings of css of every card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -37,6 +37,7 @@ import { CardsContentAudioComponent } from './components/homepage/cards-containe
 import { CardsContentImageComponent } from './components/homepage/cards-container/cards-content-image/cards-content-image.component';
 import { CardsContentTextComponent } from './components/homepage/cards-container/cards-content-text/cards-content-text.component';
 import { CardContentDetailComponent } from './components/homepage/cards-container/card-content-detail/card-content-detail.component';
+import { MatVideoModule } from 'mat-video';
 
 @NgModule({
     declarations: [
@@ -77,6 +78,7 @@ import { CardContentDetailComponent } from './components/homepage/cards-containe
         AppRoutingModule,
         ImageCropperModule,
         UserPageModule,
+        MatVideoModule,
     ],
     providers: [AngularFirestore],
     bootstrap: [AppComponent],
--- src/app/components/homepage/cards-container/cards-content-video/cards-content-video.component.ts
+++ src/app/components/homepage/cards-container/cards-content-video/cards-content-video.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
     selector: 'ita-cards-content-video',
     templateUrl: './cards-content-video.component.html',
-    styleUrls: ['./cards-content-video.component.css', '../cards-container.component.scss'],
+    styleUrls: [
+        './cards-content-video.component.css',
+        '../cards-container.component.scss',
+    ],
 })
 export class CardsContentVideoComponent implements OnInit {
     constructor() {}
