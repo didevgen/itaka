fe21238b2566338d10e7a102459d64b1fdb0bdad
starosta1721 <starosta1721@gmail.com>
2019-08-30 08:19:53 +0300

    Add some css styles for more correct image of dashboard

--- src/app/components/homepage/cards-container/cards-content-video/cards-content-video.component.ts
+++ src/app/components/homepage/cards-container/cards-content-video/cards-content-video.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
     selector: 'ita-cards-content-video',
     templateUrl: './cards-content-video.component.html',
-    styleUrls: ['./cards-content-video.component.css'],
+    styleUrls: ['./cards-content-video.component.css', '../cards-container.component.scss'],
 })
 export class CardsContentVideoComponent implements OnInit {
     constructor() {}
