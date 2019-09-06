b131e363142cdd84cb2e65a659832f43ff428c40
heilikpd <heilikpd@gmail.com>
2019-09-06 11:34:10 +0300

    FIXED bugs

--- src/app/components/editors/text-editor/text-editor.component.ts
+++ src/app/components/editors/text-editor/text-editor.component.ts
@@ -13,6 +13,8 @@ export class TextEditorComponent implements OnInit {
     private Editor = ClassicEditor;
     private config = Config;
     textContent: Text;
+    title: string;
+    content: string;
     public uploadMediaForm: FormGroup;
     constructor() {
         this.uploadMediaForm = new FormGroup({
--- src/app/components/homepage/header/header.component.ts
+++ src/app/components/homepage/header/header.component.ts
@@ -14,6 +14,8 @@ import * as AuthActions from '../../auth/store/auth.actions';
 })
 export class HeaderComponent implements OnInit, OnDestroy {
     value = 'Clear me';
+    userName: string;
+ 
     isAuthenticated = false;
     private userSub: Subscription;
 
@@ -37,4 +39,5 @@ export class HeaderComponent implements OnInit, OnDestroy {
     ngOnDestroy() {
         this.userSub.unsubscribe();
     }
+    getUserAvatar() {}
 }
