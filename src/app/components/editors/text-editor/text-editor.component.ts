51cd0bc2ccdb73d4d735b46433b20508d9e2b6bf
heilikpd <heilikpd@gmail.com>
2019-09-13 15:09:35 +0300

    remove redundant menu items

--- src/app/components/editors/text-editor/text-editor.component.ts
+++ src/app/components/editors/text-editor/text-editor.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import { Config } from './texteditor.config';
 import { AngularFirestore } from '@angular/fire/firestore';
@@ -11,8 +11,10 @@ import { GetUserIdService } from 'src/app/services/get-user-id.service';
 export class TextEditorComponent implements OnInit {
     public Editor = ClassicEditor;
     public config = Config;
+    public disabled: boolean = false;
     public title: string;
     public description: string;
+
     constructor(
         private db: AngularFirestore,
         private getUserIdService: GetUserIdService,
@@ -20,6 +22,7 @@ export class TextEditorComponent implements OnInit {
     ngOnInit(): void {}
 
     public startUpload(textContent) {
+        this.disabled = true;
         const { title, description } = textContent;
         const userId = this.getUserIdService.getUserId();
         this.db.collection('Posts').add({
