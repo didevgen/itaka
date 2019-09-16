532ef353430933327bf88eea56902eb828c2f1da
heilikpd <heilikpd@gmail.com>
2019-09-16 17:31:43 +0300

    text upload bug fixed

--- src/app/components/editors/text-editor/text-editor.component.ts
+++ src/app/components/editors/text-editor/text-editor.component.ts
@@ -4,6 +4,8 @@ import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import { Config } from './texteditor.config';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { GetUserIdService } from 'src/app/services/get-user-id.service';
+import { FormGroup, FormControl } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 @Component({
     selector: 'ita-text-editor',
     templateUrl: './text-editor.component.html',
@@ -12,46 +14,69 @@ import { GetUserIdService } from 'src/app/services/get-user-id.service';
 export class TextEditorComponent implements OnInit, OnDestroy {
     public Editor = ClassicEditor;
     public config = Config;
+    public uploadTextContentForm: FormGroup;
     public disabled: boolean;
-    public title: string;
-    public description = '';
-
+    private disableTitle: boolean = true;
+    private disableDescription: boolean = true;
     constructor(
         private db: AngularFirestore,
         private getUserIdService: GetUserIdService,
         private router: Router,
+        private snackBar: MatSnackBar,
     ) {}
     ngOnInit(): void {
-        this.disabled = false;
-        console.log(this.disabled);
-    }
-    public send() {}
-    public write({ data }) {
-        console.log(data);
-    }
-    public onChange({ editor }) {
-        const data = editor.getData();
-        data.length > 100 ? console.log(data) : console.warn(data);
-    }
-    public startUpload(textContent) {
-        console.log('send');
-        setTimeout(() => {
-            this.router.navigate(['userPage']);
-        }, 2000);
         this.disabled = true;
-        console.log(this.disabled);
-        const { title, description } = textContent;
-        const userId = this.getUserIdService.getUserId();
-        this.db.collection('Posts').add({
-            date: new Date(),
-            title,
-            description,
-            contentType: 'text',
-            userId,
+        this.uploadTextContentForm = new FormGroup({
+            title: new FormControl(),
+            description: new FormControl(),
         });
     }
-    ngOnDestroy() {
-        this.disabled = false;
-        console.log(this.disabled);
+    private checkTitle(data): void {
+        const { value } = data.target;
+        !value ? (this.disableTitle = true) : (this.disableTitle = false);
+        this.disabling();
+    }
+    private checkDescription({ editor }): void {
+        const data = editor.getData();
+        data.length <= 400 || data.length >= 700
+            ? (this.disableDescription = true)
+            : (this.disableDescription = false);
+        this.disabling();
+    }
+    private disabling(): void {
+        this.disableTitle || this.disableDescription
+            ? (this.disabled = true)
+            : (this.disabled = false);
+    }
+    private redirect(): Promise<boolean> {
+        return this.router.navigate(['userPage']);
+    }
+    public startUpload(data): void {
+        const userId = this.getUserIdService.getUserId();
+        this.db
+            .collection('Posts')
+            .add({
+                date: new Date(),
+                title: data.title,
+                description: data.description,
+                contentType: 'text',
+                userId,
+            })
+            .then(_ => {
+                this.openSnackBar('Text added');
+                setTimeout(() => {
+                    this.redirect();
+                }, 2000);
+            })
+            .catch(err => this.openSnackBar(err));
+    }
+    openSnackBar(message: string): void {
+        this.snackBar.open(message, 'Close', {
+            duration: 1500,
+        });
+    }
+
+    ngOnDestroy(): void {
+        this.disabled = true;
     }
 }
