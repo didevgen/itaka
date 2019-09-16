682e0be76679774d42b419460ff804f0e1aba592
kerrimov <tolik.kerimov@gmail.com>
2019-09-16 18:29:24 +0300

    card icons was moved to the separated component

---
+++ src/app/components/homepage/cards-container/card-buttons/card-buttons.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'ita-card-buttons',
+    templateUrl: './card-buttons.component.html',
+    styleUrls: ['./card-buttons.component.scss'],
+})
+export class CardButtonsComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit() {}
+}
