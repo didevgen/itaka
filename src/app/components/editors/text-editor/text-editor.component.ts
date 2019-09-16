c1e8a4d68747781064f6ba045aaf3d70c3506a8d
heilikpd <heilikpd@gmail.com>
2019-09-16 12:26:06 +0300

    fixed redirect bug on header component

--- src/app/components/editors/text-editor/text-editor.component.ts
+++ src/app/components/editors/text-editor/text-editor.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import { Config } from './texteditor.config';
 import { AngularFirestore } from '@angular/fire/firestore';
@@ -13,19 +14,31 @@ export class TextEditorComponent implements OnInit, OnDestroy {
     public config = Config;
     public disabled: boolean;
     public title: string;
-    public description: string = '';
-   
+    public description = '';
+
     constructor(
         private db: AngularFirestore,
         private getUserIdService: GetUserIdService,
+        private router: Router,
     ) {}
     ngOnInit(): void {
         this.disabled = false;
         console.log(this.disabled);
     }
-
+    public send() {}
+    public write({ data }) {
+        console.log(data);
+    }
+    public onChange({ editor }) {
+        const data = editor.getData();
+        data.length > 100 ? console.log(data) : console.warn(data);
+    }
     public startUpload(textContent) {
-        this.disabled = false;
+        console.log('send');
+        setTimeout(() => {
+            this.router.navigate(['userPage']);
+        }, 2000);
+        this.disabled = true;
         console.log(this.disabled);
         const { title, description } = textContent;
         const userId = this.getUserIdService.getUserId();
