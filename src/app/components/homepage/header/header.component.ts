cd29b91dc1447dc248493156b1ed43dc7c4a492a
heilikpd <heilikpd@gmail.com>
2019-09-20 17:54:36 +0300

    add search by uuid

--- src/app/components/homepage/header/header.component.ts
+++ src/app/components/homepage/header/header.component.ts
@@ -1,27 +1,14 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { appReducer } from '../../../store/app.reducer';
-import {
-    Subscription,
-    Observable,
-    Subject,
-    of,
-    BehaviorSubject,
-    from,
-} from 'rxjs';
+import { Subscription, Observable, Subject, of, fromEvent } from 'rxjs';
 import {
     map,
-    filter,
     debounceTime,
     distinctUntilChanged,
     mergeMap,
     delay,
-    multicast,
 } from 'rxjs/operators';
 import { Store } from '@ngrx/store';
-import {
-    AngularFireStorage,
-    AngularFireUploadTask,
-} from '@angular/fire/storage';
 import { AngularFirestore } from '@angular/fire/firestore';
 import * as fromApp from '../../../store/app.reducer';
 import * as AuthActions from '../../auth/store/auth.actions';
@@ -60,22 +47,29 @@ export class HeaderComponent implements OnInit, OnDestroy {
             .pipe(map(authState => authState.user))
             .subscribe(user => {
                 this.isAuthenticated = !!user;
-                console.log(!user);
-                console.log(!!user);
             });
         const sub = this.inputSearch;
         this.searchInput = sub.pipe(
-            map(event => event),
             debounceTime(300),
             distinctUntilChanged(),
             mergeMap(e => of(e).pipe(delay(500))),
         );
         const a = this.searchInput.subscribe(t => console.log('input ==>', t));
-        this.searchInput.subscribe(e => search.searchByTitle(e));
-        this.searchInput.subscribe(e => search.searchByAuthorName(e));
-        a.unsubscribe();
+        this.searchInput.subscribe((e: string) => search.searchByTitle(e));
+        this.searchInput.subscribe((e: string) => search.searchByAuthorName(e));
+        // a.unsubscribe();
     }
-
+    onSearch(event) {
+        // console.log(event.target.value);
+        // fromEvent(event.target.value, 'input')
+        //     .pipe(
+        //         map(event => event),
+        //         debounceTime(500),
+        //         distinctUntilChanged(),
+        //         mergeMap(e => of(e).pipe(delay(500))),
+        //     )
+        //     .subscribe(e => console.log('ads', e));
+    }
     onLogout() {
         this.store.dispatch(new AuthActions.Logout());
     }
--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -23,14 +23,15 @@ import {
     AngularFireUploadTask,
 } from '@angular/fire/storage';
 import { AngularFirestore } from '@angular/fire/firestore';
+// import undefined = require('firebase/empty-import');
 
 @Injectable({
     providedIn: 'root',
 })
 export class SearchService {
     constructor(private db: AngularFirestore) {}
-    searchByTitle(value) {
-        console.log(this.db.collection('Posts'));
+    searchByTitle(value: string) {
+        // console.log(this.db.collection('Posts'));
         this.db
             .collection('Posts')
             .get()
@@ -49,34 +50,56 @@ export class SearchService {
                             i
                                 .data()
                                 .title.toLowerCase()
-                                .indexOf(value.toLowerCase()) >= 0,
+                                .indexOf(value.toLowerCase()) > -1,
                     ),
                 );
             });
     }
-    searchByAuthorName(value) {
-        console.log(this.db.collection('Users'));
+    getAuthorId(name: string): string {
+        // const names: Array<object>;
+        // let users = this.db
+        //     .collection('Users')
+        //     .get()
+        //     .toPromise()
+        //     .then(user => {
+        //         user.docs.filter(u =>
+        //             u
+        //                 .data()
+        //                 .name.toLowerCase()
+        //                 .indexOf(name.toLowerCase()),
+        //         );
+        //     })
+        //     .then(e => (userId = e));
+        console.log(`this.db.collection('Users').get()`);
+        console.log(
+            this.db
+                .collection('Users')
+                .get()
+                .toPromise()
+                .then(collection =>
+                    collection.docs.filter(user => user.data().name === name),
+                ),
+        );
+        return userId;
+    }
+    searchByAuthorName(name: string) {
+        const userId = this.getAuthorId(name);
+        console.log(userId);
         this.db
-            .collection('Users')
+            .collection('Posts')
             .get()
             .toPromise()
             .then(post => {
-                post.docs.filter(
-                    i =>
-                        i
-                            .data()
-                            .title.toLowerCase()
-                            .indexOf(value.toLowerCase()) >= 0,
-                );
-                console.log(
-                    post.docs.filter(
-                        i =>
-                            i
-                                .data()
-                                .title.toLowerCase()
-                                .indexOf(value.toLowerCase()) >= 0,
-                    ),
-                );
+                post.docs.filter(i => i.data().userId === userId);
             });
+        console.log(
+            this.db
+                .collection('Posts')
+                .get()
+                .toPromise()
+                .then(post => {
+                    post.docs.filter(i => i.data().userId === userId);
+                }),
+        );
     }
 }
