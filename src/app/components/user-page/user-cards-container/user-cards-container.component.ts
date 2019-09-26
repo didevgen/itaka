46f783da7be0a32d04cf34667eca5b63a3d3b5ad
starosta1721 <starosta1721@gmail.com>
2019-09-26 16:38:11 +0300

    add click on title or body of card and call text editor

--- src/app/components/user-page/user-cards-container/user-cards-container.component.ts
+++ src/app/components/user-page/user-cards-container/user-cards-container.component.ts
@@ -3,6 +3,7 @@ import { Media } from 'src/app/models/content/Media/media.models';
 import { GetDataService } from 'src/app/services/get-data.service';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
     selector: 'ita-user-cards-container',
@@ -13,7 +14,8 @@ export class UserCardsContainerComponent implements OnInit, OnDestroy {
     media: Media[] = [];
     private destroy$ = new Subject<void>();
 
-    constructor(private getDataService: GetDataService) {}
+    constructor(private getDataService: GetDataService,
+                public router: Router, ) {}
 
     ngOnInit() {
         this.getDataService.currentMedia
@@ -23,5 +25,14 @@ export class UserCardsContainerComponent implements OnInit, OnDestroy {
     ngOnDestroy() {
         this.destroy$.next();
         this.destroy$.complete();
+    }
+    getConsole($event: any) {
+        console.log($event);
+        if ($event.target.classList.contains('mat-card-title') || $event.path[1].classList.contains('mat-card-content')) {
+        console.log('you shoot title or content');
+        return this.router.navigate(['/addText']);
+        } else {
+         return;
+        }
     }
 }
