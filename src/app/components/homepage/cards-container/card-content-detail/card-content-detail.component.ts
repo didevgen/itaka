16822e6a6bcf1bbf0114249b03ebb432e15e4e3d
Sergey <sergeyserg.sms@gmail.com>
2019-09-28 17:11:57 +0300

    update user service for getting user profile

--- src/app/components/homepage/cards-container/card-content-detail/card-content-detail.component.ts
+++ src/app/components/homepage/cards-container/card-content-detail/card-content-detail.component.ts
@@ -1,13 +1,23 @@
-import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import {
+    Component,
+    OnInit,
+    ElementRef,
+    ViewChild,
+    OnDestroy,
+} from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { UserComment } from '../../../../models/content/comment.model';
+import { GetUserService } from '../../../../shared/get-user.service';
+import { EditProfile } from '../../../../models/edit-profile/edit-profile.model';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
     selector: 'ita-card-content-detail',
     templateUrl: './card-content-detail.component.html',
     styleUrls: ['./card-content-detail.component.scss'],
 })
-export class CardContentDetailComponent implements OnInit {
+export class CardContentDetailComponent implements OnInit, OnDestroy {
     title: string;
     description: string;
     url: string;
@@ -21,18 +31,43 @@ export class CardContentDetailComponent implements OnInit {
     isComment: boolean;
     commentFC: FormControl;
     date: string;
+    userId: string;
+    userProfile: EditProfile;
+    avatar: string;
+    private destroy$ = new Subject<void>();
+    defaultImage = '../../assets/avatarDefault.png';
 
     @ViewChild('comment', { static: false })
     comment: ElementRef;
 
-    constructor() {}
+    constructor(private userService: GetUserService) {}
 
     ngOnInit(): void {
+        this.userId = this.userService.getUserId();
+        this.userService
+            .gerUserProfile(this.userId)
+            .pipe(takeUntil(this.destroy$))
+            .subscribe(
+                profile => {
+                    this.userProfile = profile;
+                    console.log(this.userProfile, 'userProfile onInit');
+                },
+                err => console.error('error onInit subscribe ', err),
+            );
+        if (this.userProfile) {
+            this.avatar = this.userProfile.avatar;
+            console.log(this.avatar, 'avatar');
+        }
+
         this.commentFC = new FormControl('', [
             Validators.required,
             Validators.minLength(1),
             Validators.maxLength(400),
         ]);
+    }
+    ngOnDestroy(): void {
+        this.destroy$.next();
+        this.destroy$.complete();
     }
 
     onSend() {
--- src/app/shared/get-user.service.ts
+++ src/app/shared/get-user.service.ts
@@ -1,14 +1,21 @@
 import { OnDestroy } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Subscription, throwError } from 'rxjs';
+import { Observable, of, Subscription, throwError } from 'rxjs';
 import * as fromApp from '../store/app.reducer';
+import { User } from '../models/user/User.models';
+import { map } from 'rxjs/operators';
+import { EditProfile } from '../models/edit-profile/edit-profile.model';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 export class GetUserService implements OnDestroy {
     private userID: string;
     private subscription: Subscription;
     private isExisting: boolean;
 
-    constructor(private store: Store<fromApp.AppState>) {
+    constructor(
+        private store: Store<fromApp.AppState>,
+        private db: AngularFirestore,
+    ) {
         this.isExisting = false;
     }
 
@@ -26,9 +33,23 @@ export class GetUserService implements OnDestroy {
         if (this.isExisting) {
             return this.userID;
         } else {
-            throwError(`user doesn't exist!`);
+            throwError(`user doesn't exist! (from service)`);
         }
     }
+
+    gerUserProfile(id): Observable<EditProfile> {
+        return this.db
+            .collection<User>('Users')
+            .doc(id)
+            .valueChanges()
+            .pipe(
+                map(
+                    val => val as EditProfile,
+                    throwError('error in service getUserProfile'),
+                ),
+            );
+    }
+
     ngOnDestroy() {
         this.subscription.unsubscribe();
         fromApp.getUser.release();
