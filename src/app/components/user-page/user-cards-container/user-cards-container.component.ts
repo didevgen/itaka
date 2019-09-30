8598711d9123f0d2770e1217203ceb2cfcc06d61
heilikpd <heilikpd@gmail.com>
2019-09-30 21:13:10 +0300

    add search by content of specific user

--- src/app/components/user-page/user-cards-container/user-cards-container.component.ts
+++ src/app/components/user-page/user-cards-container/user-cards-container.component.ts
@@ -3,6 +3,7 @@ import { Media } from 'src/app/models/content/Media/media.models';
 import { GetDataService } from 'src/app/services/get-data.service';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
+import { SearchService } from 'src/app/services/search.service';
 
 @Component({
     selector: 'ita-user-cards-container',
@@ -10,15 +11,32 @@ import { takeUntil } from 'rxjs/operators';
     styleUrls: ['./user-cards-container.component.scss'],
 })
 export class UserCardsContainerComponent implements OnInit, OnDestroy {
-    media: Media[] = [];
+    public media: Media[] = [];
+    private currentMedia: Media[] = [];
     private destroy$ = new Subject<void>();
 
-    constructor(private getDataService: GetDataService) {}
+    constructor(
+        private getDataService: GetDataService,
+        private searchService: SearchService,
+    ) {}
 
     ngOnInit() {
         this.getDataService.currentMedia
             .pipe(takeUntil(this.destroy$))
-            .subscribe(content => (this.media = content));
+            .subscribe(content => {
+                this.media = content;
+                this.currentMedia = content;
+            });
+
+        this.searchService.currentSearchResponse.subscribe(
+            (foundData: Media[]) => {
+                foundData === undefined ||
+                foundData === [] ||
+                foundData.length === this.currentMedia.length
+                    ? (this.media = this.currentMedia)
+                    : (this.media = foundData);
+            },
+        );
     }
     ngOnDestroy() {
         this.destroy$.next();
