6b7a214913363766d1c6e1dac788494278782bc3
kerrimov <tolik.kerimov@gmail.com>
2019-10-02 17:44:57 +0300

    deleting updated

--- src/app/services/delete-data.service.ts
+++ src/app/services/delete-data.service.ts
@@ -1,26 +1,22 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
-import { AngularFireStorage } from '@angular/fire/storage';
 import { from, Observable } from 'rxjs';
 
 @Injectable({
     providedIn: 'root',
 })
 export class DeleteDataService {
-    constructor(
-        private db: AngularFirestore,
-        private storage: AngularFireStorage,
-    ) {}
-
-    public deleteData(postId: string): Observable<any> {
-        const deleteFromDb = this.deleteDataFromDb(postId);
-        return from(deleteFromDb);
-    }
+    constructor(private db: AngularFirestore) {}
 
     private deleteDataFromDb(postId: string): Promise<any> {
         return this.db
             .collection('Posts')
             .doc(postId)
             .delete();
+    }
+
+    public deleteData(postId: string): Observable<any> {
+        const deleteFromDb = this.deleteDataFromDb(postId);
+        return from(deleteFromDb);
     }
 }
